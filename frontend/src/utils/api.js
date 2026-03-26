cccc1f08f806cdc3eceaa6007b0835129087722e
aditya4NEchoTech <aadityakasaudhan2002@gmail.com>
2026-03-26 16:14:22 +0530

    Updated the api name in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:5000/api';
-const BASE_URL = 'https://jobby-1j92.onrender.com';
+const BASE_URL = 'https://jobby-1j92.onrender.com/api';
 
 const api = axios.create({
     baseURL: BASE_URL,
