f23dbcb2b7530f6eaace01941c04b4a7803a2391
aditya4NEchoTech <aadityakasaudhan2002@gmail.com>
2026-03-26 15:44:30 +0530

    Deployed the website on the render

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://jobby-1j92.onrender.com';
 
 const api = axios.create({
     baseURL: BASE_URL,
